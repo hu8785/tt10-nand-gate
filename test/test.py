b551c8b4ed87ce880087ae4ddccad802fd4bbefe
hu8785 <patilaku96@gmail.com>
2025-12-02 23:40:52 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -11,7 +8,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -25,16 +22,30 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+# Set the input values you want to test
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 0
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+# Wait for a certain number of clock cycles as you choose to see the output values
+    await ClockCycles(dut.clk, 25)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+# The following assertion is just an example of how to check the output values.
+   # Change it to match the actual expected output of your module:
+    assert dut.uo_out[0].value == 1
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 1
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 1
+
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 0
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 1
+
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 1
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 0
+# Keep testing the module by changing the input values, waiting for
+# one or more clock cycles, and asserting the expected output values.
